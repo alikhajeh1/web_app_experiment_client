9d4061a166690e4d026f40dc2cf84ef8720740c2
James Smith <james.w.smith@st-andrews.ac.uk>
2012-02-14 15:30:08 +0000

    Adjusted timestamp taking to do once for each check, not each line

--- HistoryMonitor.py
+++ HistoryMonitor.py
@@ -11,21 +11,22 @@ from FileIO import read as read
 # Time/Date object
 import datetime
 from datetime import timedelta
-now = datetime.datetime.now()
 
 def checkHistory():
   print 'checkHistory method'
   history = read("history.csv")
   entries = history.split('\n')
+
+  # Now define a Timestamp string for the last hour
+  now = datetime.datetime.now()
+  lastHour = now - timedelta(hours=1)
+  print 'Last hour: ' + str(lastHour)
+
   for entry in entries[1:]: # Ignore headers
 
     # Extract values -> values[0]=id, values[1]=time, values[2]=level
     values = entry.split(',') 
-    
-    # Now define a Timestamp string for the last hour
-    lastHour = now - timedelta(hours=1)
-    print 'Last hour: ' + str(lastHour)
-    
+
     # If current entry was in the last hour but less than now
     if values[1] > str(lastHour):
       if values[1] < str(now):
@@ -34,6 +35,7 @@ def checkHistory():
   
   # If we have found no entry in the last hour  
   # return a signal
+  print 'No entries in the last hour'
   return 'current'
 
 def main():
