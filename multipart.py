5caac16de1b88c31a21663dad98b630585bf9433
James Smith <james.w.smith@st-andrews.ac.uk>
2012-02-14 09:35:44 +0000

    Added code to do multipart POST uploads

---
+++ multipart.py
@@ -0,0 +1,67 @@
+import httplib, mimetypes   
+import base64
+import string
+
+
+"""
+From: Http client to POST using multipart/form-data (Python recipe)
+
+http://code.activestate.com/recipes/146306/
+"""
+def post_multipart(host, selector, fields, files):
+
+    print 'Connecting to host: ' + host
+    
+    userid = "username"
+    passwd = "password"
+    # "Basic" authentication encodes userid:password in base64. Note
+    # that base64.encodestring adds some extra newlines/carriage-returns
+    # to the end of the result. string.strip is a simple way to remove
+    # these characters.
+    auth = 'Basic ' + string.strip(base64.encodestring(userid + ':' + passwd))
+
+    """
+    Post fields and files to an http host as multipart/form-data.
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return the server's response page.
+    """
+    content_type, body = encode_multipart_formdata(fields, files)
+    h = httplib.HTTP(host)
+    h.putrequest('POST', "/videos")
+    #h.putheader('Authorization', auth )
+    h.putheader('content-type', content_type)
+    h.putheader('content-length', str(len(body)))
+    h.endheaders()
+    h.send(body)
+    errcode, errmsg, headers = h.getreply()
+    return h.file.read()
+
+def encode_multipart_formdata(fields, files):
+    """
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return (content_type, body) ready for httplib.HTTP instance
+    """
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []
+    for (key, value) in fields:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"' % key)
+        L.append('')
+        L.append(value)
+    for (key, filename, value) in files:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('')
+        L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
