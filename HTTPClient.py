fa96608054c52edccd138e03d1e15c62fef1a485
James Smith <james.w.smith@st-andrews.ac.uk>
2012-02-16 17:51:31 +0000

    Changed the Python client to POST the WMV video

--- HTTPClient.py
+++ HTTPClient.py
@@ -142,7 +142,7 @@ class HTTPClient(threading.Thread):
     fields.append(('video[description]', str(now)))
     
     # Read file
-    f = open('bbc_two.mp4', 'r')    
+    f = open('ELL_PART_5_768k.wmv', 'r')    
     data = f.read()
     f.close()
     self.txBytes += len(data)
