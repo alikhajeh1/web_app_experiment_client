d315f7ba5a7586ad43b2d03b2bc1d03623e963cc
James Smith <james.w.smith@st-andrews.ac.uk>
2012-02-16 08:18:48 +0000

    Fixed int to string bug

--- Client.py
+++ Client.py
@@ -117,7 +117,7 @@ class Client(threading.Thread):
   
       now = datetime.datetime.now()
       print "[Client:behaviour] Current date and time:" + str(now)
-      print "[Client:behaviour] Current concurrent thread count:" + len(self.threads)
+      print "[Client:behaviour] Current concurrent thread count:" + str(len(self.threads))
 
       # Now parse the Config File and show the current thread level counts
       parseConfig()
